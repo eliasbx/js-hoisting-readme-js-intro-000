4a105eb3168333510e28eac3abbc1cba0c9c1012
Elias Felvinczi <eliasfelvinczi@gmail.com>
2020-05-11 17:26:14 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,7 +11,7 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  
+
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
@@ -27,7 +27,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    
+
     var name = "Kristin";
     console.log(name);
   }
