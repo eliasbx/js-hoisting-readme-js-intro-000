6ee7e967599d1ee9381e168f30a7d3ccf3f3bde7
Elias Felvinczi <eliasfelvinczi@gmail.com>
2020-05-11 17:25:28 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,13 +11,14 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
-crazy()();
+crazy();
 
 function sayMyName() {
   // fix the code in here:
